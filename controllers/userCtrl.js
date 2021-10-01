dd4153c540f65f7e7e40ebbd2307588dc7b23afd
dilwarsingh24 <dilwarinstasoftservices@gmail.com>
2021-10-01 15:10:22 +0530

    first commit

--- controllers/userCtrl.js
+++ controllers/userCtrl.js
@@ -151,8 +151,8 @@ const userCtrl = {
                 subject: "Keshu Channel",
                 html: `
                     <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-                    <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the DevAT channel.</h2>
-                    <p>Congratulations! You're almost set to start using DEVAT✮SHOP.
+                    <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to the KESHU SHOP .</h2>
+                    <p>Congratulations! You're almost set to start using KESHU✮SHOP.
                         Just click the button below to validate your email address.
                     </p>
                     
